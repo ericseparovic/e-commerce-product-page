ae9d071cb19e839e5cee1b0c43f71247b3079158
Eric Separovic <ericseparovic07@gmail.com>
2022-11-22 11:17:14 -0300

    Create component Main section products

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import IconMenu from "./assets/icon-menu.svg";
-import LogoSneakers from "./assets/logo.svg";
-import IconCart from "./assets/icon-cart.svg";
-import Avatar from "./assets/image-avatar.png";
-import IconClose from "./assets/icon-close.svg";
 import Header from "./components/Header";
+import Main from "./components/Main";
 
 function App() {
   return (
     <>
       <Header />
+      <Main />
     </>
   );
 }
