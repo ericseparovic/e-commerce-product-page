5c9233740ac6348b76eff1b757634c420d90c22a
Eric Separovic <ericseparovic07@gmail.com>
2022-12-12 08:44:10 -0300

    Add carousel version desktop

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -6,6 +6,10 @@ import ImgProduct4 from "../assets/image-product-4.jpg";
 import IconPrevious from "./Icons/IconPrevious";
 import IconNext from "./Icons/IconNext";
 import React from "react";
+import ImgProduct1Thumbnail from "../assets/image-product-1.jpg";
+import ImgProduct2Thumbnail from "../assets/image-product-2.jpg";
+import ImgProduct3Thumbnail from "../assets/image-product-3.jpg";
+import ImgProduct4Thumbnail from "../assets/image-product-4.jpg";
 
 function Carousel() {
   let position = 0;
@@ -23,16 +27,40 @@ function Carousel() {
   };
 
   return (
-    <section className="carousel">
+    <div className="container__carousel">
       <IconPrevious handlePrevious={handlePrevious} />
       <IconNext handleNext={handleNext} />
-      <section className="main__products">
-        <img className="main__img-product" src={ImgProduct1} alt="Product 1" />
-        <img className="main__img-product" src={ImgProduct2} alt="Product 2" />
-        <img className="main__img-product" src={ImgProduct3} alt="Product 3" />
-        <img className="main__img-product" src={ImgProduct4} alt="Product 4" />
+      <section className="carousel">
+        <section className="main__products">
+          <img
+            className="main__img-product"
+            src={ImgProduct1}
+            alt="Product 1"
+          />
+          <img
+            className="main__img-product"
+            src={ImgProduct2}
+            alt="Product 2"
+          />
+          <img
+            className="main__img-product"
+            src={ImgProduct3}
+            alt="Product 3"
+          />
+          <img
+            className="main__img-product"
+            src={ImgProduct4}
+            alt="Product 4"
+          />
+        </section>
       </section>
-    </section>
+      <div className="main__product-gallery">
+        <img src={ImgProduct1Thumbnail} alt="img-1" />
+        <img src={ImgProduct2Thumbnail} alt="img-2" />
+        <img src={ImgProduct3Thumbnail} alt="img-3" />
+        <img src={ImgProduct4Thumbnail} alt="img-4" />
+      </div>
+    </div>
   );
 }
 
